067d42b12f62ad808d4ae494437c1c67b0118c2e
Renata Jegereva <github@webcreating.co.uk>
2018-05-23 11:24:51 +0100

    feat: add tab icons

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 // import { createStore } from 'redux'
 // import { Provider } from 'react-redux'
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
-import { purple } from './utils/colours'
+import { purple, white } from './utils/colours'
 import DecksList from './components/DecksList'
 import AddDeck from './components/AddDeck'
 import { Constants } from 'expo'
+import { MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons'
 
 function FlashcardsStatusBar ({backgroundColor, ...props}) {
   return (
@@ -16,22 +17,67 @@ function FlashcardsStatusBar ({backgroundColor, ...props}) {
   )
 }
 
+
 const Tabs = createBottomTabNavigator({
   DecksList: {
     screen: DecksList,
     navigationOptions: {
       title: 'Decks List',
-      tabBarLabel: 'Decks List'
+      tabBarLabel: 'Decks List',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-playing-outline' size={30} color={tintColor} />
+      // tabBarIcon: () => <MaterialCommunityIcons name='cards' size={30} />
     }
   },
   AddDeck: {
     screen: AddDeck,
     navigationOptions: {
       title: 'Add Deck',
-      tabBarLabel: 'Add Deck'
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+      // tabBarIcon: () => <MaterialCommunityIcons name='cards-playing-outline' size={30} />
     }
   }
-});
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+
+// const Tabs = createBottomTabNavigator({
+//   DecksList: {
+//     screen: DecksList,
+//     navigationOptions: {
+//       title: 'Decks List',
+//       tabBarLabel: 'Decks List',
+//       tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards-playing-outline' size={30} color={tintColor} />
+//       // tabBarIcon: () => <MaterialCommunityIcons name='cards' size={30} />
+//     }
+//   },
+//   AddDeck: {
+//     screen: AddDeck,
+//     navigationOptions: {
+//       title: 'Add Deck',
+//       tabBarLabel: 'Add Deck',
+//       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+//       // tabBarIcon: () => <MaterialCommunityIcons name='cards-playing-outline' size={30} />
+//     }
+//   }
+// });
 
 const MainNavigator = createStackNavigator({
   Home: {
