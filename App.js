fd53421334001797d8c563141bd47f45dd1f57df
Renata Jegereva <github@webcreating.co.uk>
2018-05-31 16:05:51 +0100

    chore

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { composeWithDevTools } from 'remote-redux-devtools'
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native'
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
@@ -8,7 +7,7 @@ import AddDeck from './components/AddDeck'
 import { Constants } from 'expo'
 import { MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware  } from 'redux'
+import { compose, createStore, applyMiddleware  } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from './reducers'
 
@@ -21,21 +20,16 @@ const logger = store => next => action => {
   return result
 }
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-// const store = createStore(
-//   reducer,
-//   composeEnhancers(
-//     applyMiddleware(thunk, logger)
-//   )
-// )
-const composeEnhancers = composeWithDevTools({ realtime: true, port: 19001 });
-const store = createStore(reducer, composeEnhancers(
-  applyMiddleware(thunk, logger)
-));
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
+)
 
-console.log('STORE: ' + JSON.stringify(store.getState()))
-
+// console.log('STORE: ' + JSON.stringify(store.getState()))
 
 function FlashcardsStatusBar ({backgroundColor, ...props}) {
   return (
